425515bc53d50a3bea54dc88373d8a7c8eb77bbe
miraecho <49175337+miraecho@users.noreply.github.com>
2026-07-23 22:00:10 -0700

    Polished Waypoint Transition

--- Assets/MapTransition.cs
+++ Assets/MapTransition.cs
@@ -7,10 +7,14 @@ public class MapTransition : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundary;
     CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePosition = 3f;
+
+    enum Direction { Up, Down, Left, Right }
 
     private void Awake()
     {
-        confiner = FindObjectOfType<CinemachineConfiner2D>();
+        confiner = FindAnyObjectByType<CinemachineConfiner2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,6 +22,30 @@ public class MapTransition : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
         }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPosition = player.transform.position;
+
+        switch (direction) 
+        {
+            case Direction.Up:
+                newPosition.y += additivePosition;
+                break;
+            case Direction.Down:
+                newPosition.y -= additivePosition;
+                break;
+            case Direction.Left:
+                newPosition.x += additivePosition;
+                break;
+            case Direction.Right:
+                newPosition.x -= additivePosition;
+                break;
+        }
+        
+        player.transform.position = newPosition;
     }
 }
