a297e391b9f6377e6c765268fe6e221e84c62a1c
miraecho <49175337+miraecho@users.noreply.github.com>
2026-07-24 21:56:37 -0700

    UI Tab Switching

---
+++ Assets/MenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
