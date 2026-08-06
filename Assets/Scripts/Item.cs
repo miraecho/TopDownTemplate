77aadeb05d7f75d2706cec83d13c23245b2cbcb7
miraecho <49175337+miraecho@users.noreply.github.com>
2026-08-05 21:58:03 -0700

    Tested Item Popups and Dequeueing Inventory

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public int ID;
+    public string Name;
+
+    public virtual void PickUp() 
+    {
+        Sprite itemIcon = GetComponent<SpriteRenderer>().sprite;
+        if (ItemPickupUIController.Instance != null) 
+        {
+            ItemPickupUIController.Instance.ShowItemPickup(Name, itemIcon);
+        }
+    }
 }
