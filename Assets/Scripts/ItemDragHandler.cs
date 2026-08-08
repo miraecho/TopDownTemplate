ee8d37dc684e4a626a8dec10fd641b271d90fee0
miraecho <49175337+miraecho@users.noreply.github.com>
2026-08-07 21:55:57 -0700

    Dropping Items from Inventory

--- Assets/Scripts/ItemDragHandler.cs
+++ Assets/Scripts/ItemDragHandler.cs
@@ -9,6 +9,9 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     Transform originalParent;
     CanvasGroup canvasGroup;
 
+    public float minDropDistance = 2f;
+    public float maxDropDistance = 3f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -63,11 +66,50 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         }
         else 
         {
-            //No slot under drop point
-            transform.SetParent(originalParent);
+            //If where we're dropping is not within the inventory
+            if (!IsWithinInventory(eventData.position)) 
+            {
+                //Drop our item
+                DropItem(originalSlot);
+            }
+            else 
+            {
+                //Snap back to og slot
+                transform.SetParent(originalParent);
+            }          
         }
 
         GetComponent<RectTransform>().anchoredPosition = Vector2.zero; //Center
+    }
+
+    
+    bool IsWithinInventory(Vector2 mousePosition) 
+    {
+        RectTransform inventoryRect = originalParent.parent.GetComponent<RectTransform>();
+        return RectTransformUtility.RectangleContainsScreenPoint(inventoryRect, mousePosition);
+    }
+
+    void DropItem(Slot originalSlot) 
+    {
+        originalSlot.currentItem = null;
+
+        //Find player
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
+        if (playerTransform == null) 
+        {
+            Debug.LogError("Missing 'Player' tag");
+            return;
+        }
+        //Random drop position
+        Vector2 dropOffset = Random.insideUnitCircle.normalized * Random.Range(minDropDistance, maxDropDistance);
+        Vector2 dropPosition = (Vector2)playerTransform.position + dropOffset;
+
+        //Instantiate drop item
+        GameObject dropItem = Instantiate(gameObject, dropPosition, Quaternion.identity);
+        dropItem.GetComponent<BounceEffect>().StartBounce();
+
+        //Destroy the UI one
+        Destroy(gameObject);
     }
 
 }
