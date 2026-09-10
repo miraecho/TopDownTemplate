da8e1fdb54951812eae72f1134ded1f9f11f3cb9
miraecho <49175337+miraecho@users.noreply.github.com>
2026-09-09 21:53:49 -0700

    Continued to Add to Dialogue Branching

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -11,6 +11,8 @@ public class DialogueController : MonoBehaviour
     public GameObject dialoguePanel;
     public TMP_Text dialogueText, nameText;
     public Image portraitImage;
+    public Transform choiceContainer;
+    public GameObject choiceButtonPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -33,6 +35,18 @@ public class DialogueController : MonoBehaviour
     public void SetDialogueText(string text) 
     {
         dialogueText.text = text;
+    }
+
+    public void ClearChoices() 
+    {
+        foreach(Transform child in  choiceContainer) Destroy(child.gameObject);
+    }
+
+    public void CreateChoiceButton(string choiceText, UnityEngine.Events.UnityAction onClick) 
+    {
+        GameObject choiceButton = Instantiate(choiceButtonPrefab, choiceContainer);
+        choiceButton.GetComponentInChildren<TMP_Text>().text = choiceText;
+        choiceButton.GetComponent<Button>().onClick.AddListener(onClick);
     }
  
 }
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -45,7 +45,7 @@ public class NPC : MonoBehaviour, IInteractable
         dialogueUI.ShowDialogueUI(true);
         PauseController.SetPause(true);
 
-        StartCoroutine(TypeLine());
+        DisplayCurrentLine();
     }
 
     void NextLine() 
@@ -57,10 +57,30 @@ public class NPC : MonoBehaviour, IInteractable
             dialogueUI.SetDialogueText(dialogueData.dialogueLines[dialogueIndex]);
             isTyping = false;
         }
-        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+
+        //Clear existing choices
+        dialogueUI.ClearChoices();
+        //Check endDialogueLines
+        if (dialogueData.endDialogueLines.Length > dialogueIndex && dialogueData.endDialogueLines[dialogueIndex]) 
         {
-            //If another line, type the next line
-            StartCoroutine(TypeLine());
+            EndDialogue();
+            return;
+        }
+
+        //Check if there are choices and display
+        foreach (Dialoguechoice dialogueChoice in dialogueData.choices) 
+        {
+            if (dialogueChoice.dialogueIndex == dialogueIndex) 
+            {
+                DisplayChoices(dialogueChoice);
+                return;
+            }
+        }
+
+
+        if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            DisplayCurrentLine();
         }
         else 
         {
@@ -89,6 +109,28 @@ public class NPC : MonoBehaviour, IInteractable
         }
     }
 
+    void DisplayChoices(Dialoguechoice choice) 
+    {
+        for (int i = 0; i < choice.choices.Length; i++) 
+        {
+            int nextIndex = choice.nextDialogueIndexes[i];
+            dialogueUI.CreateChoiceButton(choice.choices[i], () => ChooseOption(nextIndex));
+        }
+    }
+
+    void ChooseOption(int nextIndex) 
+    {
+        dialogueIndex = nextIndex;
+        dialogueUI.ClearChoices();
+        DisplayCurrentLine();
+    }
+
+    void DisplayCurrentLine() 
+    {
+        StopAllCoroutines();
+        StartCoroutine(TypeLine());
+    }
+    
     public void EndDialogue() 
     {
         StopAllCoroutines();
